9b6e838bcc3a598ebbb795e9ca79255a52743dd2
Markus Pfeifer <mkspfeifer@gmail.com>
2026-04-08 09:56:28 +0200

    compile.sh now automatically removes old PDFs

--- compile.sh
+++ compile.sh
@@ -9,6 +9,7 @@ fi
 LATEST_TAG=$(git describe --tags --abbrev=0)
 TANZ="${HOME}/C-Team/Bilder/OneChance/${LATEST_TAG:1}_Trainersicht_OneChance.pdf"
 TRAIN="${HOME}/C-Team/Bilder/OneChance/${LATEST_TAG:1}_Tanzsicht_OneChance.pdf"
+rm $HOME/C-Team/Bilder/OneChance/*.pdf
 coco OneChance.choreo $TANZ
 coco OneChance.choreo $TRAIN --topUp
 ./bookmark.py $TANZ "OneChance.choreo"
